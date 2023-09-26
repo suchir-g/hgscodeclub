d25ec41f87085ec49db2feb6e9ce2f8cefc9846e
dunkshotenthusiast <18sgupta@heckgrammar.co.uk>
2023-09-26 22:15:51 +0100

    git!!!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,19 @@ import "./css/Home.css";
 export default function Home({ isAuth }) {
   return (
     <div className="container">
-      <div className="welcomeDiv">
-        <h1 className="welcome">Welcome to code club</h1>
-        <p>Write commands in the terminal below to nagivate the site</p>
+      <div className="mainContent">
+        <div className="welcomeDiv">
+          <div className="miniNestedDiv">
+            <h1 className="welcome welcome-hi">Hi.</h1>
+            <h1 className="welcome welcome-lt">Welcome to code club.</h1>
+            <br />
+            <i className="subtitle">
+              {">"} Write commands in the terminal below to nagivate the site
+            </i>
+          </div>
+        </div>
+        <Terminal isAuth={isAuth} />
       </div>
-      <Terminal isAuth={isAuth} />
     </div>
   );
 }
--- src/pages/Terminal/Terminal.js
+++ src/pages/Terminal/Terminal.js
@@ -4,7 +4,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function Terminal({ isAuth }) {
   const [input, setInput] = useState("");
-  const [output, setOutput] = useState("");
+  const [testNewOutput, setTestNewOutput] = useState([]);
+  const [color, setColor] = useState("");
   const inputRef = useRef();
   const navigate = useNavigate();
 
@@ -14,93 +15,102 @@ export default function Terminal({ isAuth }) {
 
   return (
     <div className="terminalHolder">
-      <div className="terminalOutput">{output || "help clist problems login register post home clear"}</div>
+      <div className="terminalOutput" style={{ color: "#e5e5e5" }}>
+        {testNewOutput.map((d) => (
+          <p>
+            <span style={{ color: "#bc3fbc" }}>$ </span> {d}
+          </p>
+        ))}
+        {!testNewOutput && "help clist problems login register post home clear"}
+      </div>
       <div className="terminalInput">
-        <input
-          className="terminalInputSingle"
-          ref={inputRef}
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              let newOutput = "";
-              newOutput = output + "\n\n" + "$ " + input + "\n";
-              switch (input) {
-                case "help":
-                  newOutput +=
-                    "Use the terminal to get around this website. The commands will be listed for you if you type 'clist' into the text box. \n\n If you are still struggling, hover at the top of the screen to access the menu.";
-                  break;
-                case "clist":
-                  newOutput +=
-                    "problems: lists the problems \n weekly: shows the weekly problem \n login: login page \n profile: profile page \n users: look at all users \n about-us: tells you about us";
-                  break;
-                case "problems":
-                  newOutput += "Navigating to problems";
-                  navigate("/problems");
-                  break;
-                case "home":
-                  newOutput += "Navigating to home";
-                  navigate("/");
-                  break;
-                case "login":
-                  if (isAuth) {
+        <div>
+          <span style={{ color: "#f2f245" }}>$</span>{" "}
+          <input
+            className="terminalInputSingle"
+            ref={inputRef}
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                let newOutput = input + "\n";
+                switch (input) {
+                  case "help":
                     newOutput +=
-                      "You are already logged in. Did you mean 'profile'?";
-                  } else {
-                    newOutput += "Navigating to login";
-                    navigate("/login");
-                  }
-                  break;
-                case "signup":
-                  if (!isAuth) {
-                    newOutput += "Navigating to signup";
-                    navigate("/register");
-                  } else {
+                      "Use the terminal to get around this website. The commands will be listed for you if you type 'clist' into the text box. \n\n If you are still struggling, hover at the top of the screen to access the menu.";
+                    break;
+                  case "clist":
                     newOutput +=
-                      "You are already logged in. Did you mean 'profile'?";
-                  }
-                  break;
-                case "register":
-                  if (!isAuth) {
-                    newOutput += "Navigating to signup";
-                    navigate("/register");
-                  } else {
+                      "problems: lists the problems \n weekly: shows the weekly problem \n login: login page \n profile: profile page \n users: look at all users \n about-us: tells you about us";
+                    break;
+                  case "problems":
+                    newOutput += "Navigating to problems";
+                    navigate("/problems");
+                    break;
+                  case "home":
+                    newOutput += "Navigating to home";
+                    navigate("/");
+                    break;
+                  case "login":
+                    if (isAuth) {
+                      newOutput +=
+                        "You are already logged in. Did you mean 'profile'?";
+                    } else {
+                      newOutput += "Navigating to login";
+                      navigate("/login");
+                    }
+                    break;
+                  case "signup":
+                    if (!isAuth) {
+                      newOutput += "Navigating to signup";
+                      navigate("/register");
+                    } else {
+                      newOutput +=
+                        "You are already logged in. Did you mean 'profile'?";
+                    }
+                    break;
+                  case "register":
+                    if (!isAuth) {
+                      newOutput += "Navigating to signup";
+                      navigate("/register");
+                    } else {
+                      newOutput +=
+                        "You are already logged in. Did you mean 'profile'?";
+                    }
+                  case "weekly":
+                    newOutput += "we don# have that yet mb";
+                  case "profile":
+                    if (isAuth) {
+                      newOutput += "Navigating to profile";
+                      navigate("/profile");
+                    } else {
+                      newOutput +=
+                        "You aren't logged in yet. Did you mean 'login'?";
+                    }
+                    break;
+                  case "clear":
+                    newOutput = "$ " + input;
+                    break;
+                  case "ayan":
+                    newOutput += "Go back FOREST!";
+                    break;
+                  case "post":
+                    navigate("/newProblem");
+                    break;
+                  case "create":
+                    navigate("/newProblem");
+                  default:
                     newOutput +=
-                      "You are already logged in. Did you mean 'profile'?";
-                  }
-                case "weekly":
-                  newOutput += "we don# have that yet mb"
-                case "profile":
-                  if (isAuth) {
-                    newOutput += "Navigating to profile";
-                    navigate("/profile");
-                  } else {
-                    newOutput +=
-                      "You aren't logged in yet. Did you mean 'login'?";
-                  }
-                  break;
-                case "clear":
-                  newOutput = "$ " + input;
-                  break;
-                case "ayan":
-                  newOutput += "Go back FOREST!";
-                  break;
-                case "post":
-                  navigate("/newProblem")
-                  break;
-                case "create":
-                  navigate("/newProblem")
-                default:
-                  newOutput +=
-                    "Unknown command. Type 'clist' for a list of all commands.";
-                  break;
+                      "Unknown command. Type 'clist' for a list of all commands.";
+                    break;
+                }
+                setTestNewOutput([newOutput + "\n", ...testNewOutput]);
+                setInput("");
               }
-              setOutput(newOutput);
-              setInput("");
-            }
-          }}
-        />
+            }}
+          />
+        </div>
       </div>
     </div>
   );
